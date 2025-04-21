9a578dfe8fcf4aa40a0cb4401221f561a4b4a186
omaissaeed3 <omaissaeed3@gmail.com>
2025-04-21 17:40:12 -0400

    Loaded and preprocessed image datasets for Pix2Pix GAN

--- pix2pix.py
+++ pix2pix.py
@@ -1,1 +1,30 @@
-#Installed required Libraries
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+satellite_dataset = ImageFolder(root="./data/satellite", transform=transform)
+map_dataset = ImageFolder(root="./data/map", transform=transform)
+
+batch_size = 1
+satellite_loader = DataLoader(satellite_dataset, batch_size=batch_size, shuffle=True)
+map_loader = DataLoader(map_dataset, batch_size=batch_size, shuffle=True)
+
+# Show sample
+sample_image, _ = satellite_dataset[0]
+sample_image = sample_image * 0.5 + 0.5  # De-normalize
+plt.imshow(sample_image.permute(1, 2, 0))
+plt.title("Sample Image - Satellite Domain")
+plt.axis("off")
+plt.show()
+
+
